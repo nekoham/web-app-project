ea97f06d398f66aff5d38f94e4bd2ffe6891bf75
MrWasin <wasin.thao@gmail.com>
2023-04-19 17:24:18 +0700

    add Data Folder

---
+++ WebAppProject/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppProject.Models;
+
+namespace BasicASPTutorial.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
